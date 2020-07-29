ab3d15dfddf9a3da2c709522ac9f21af945a8459
Ramella Basenko <ramellabasenko@gmail.com>
2020-07-29 09:53:26 +0300

    Updated Vault task and Node app with changes
    Signed-off-by: Ramella Basenko <ramellabasenko@gmail.com>

--- Vault_task/myapp.js
+++ Vault_task/myapp.js
@@ -11,11 +11,8 @@ server.listen(PORT, HOST, function(req, res){
     console.log(`server listen on ${HOST}:${PORT}`);
 });
 server.on('request', (request, response) => {
-    // connect to the database from here which is going to be available on host DB
-    // or, which is even better. connect to vault. grab the db connection string which is a secret
-    // see https://www.npmjs.com/package/node-vault
     var pgp = require('pg-promise')(/* options */)
-    var db = pgp('postgres://postgres:postgres@DB:5432/postgres')
+    var db = pgp(`postgres://user:password@db:5432/postgres`)
     db.one('SELECT id FROM public.links AS value', 123)
       .then(function (data) {
         console.log('DATA:', data)
